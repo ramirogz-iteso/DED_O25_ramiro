f81dd32ba563cfd691cee1d0837c17720f26fba5
ramirogz <ramirogz@iteso.mx>
2025-10-02 07:05:50 -0600

    oct2

---
+++ malloc2d.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Lets create a 2D INT matrix of 5 columns and 3 rows 
+    [1, 2, 3, 4,  5]
+    [6, 7, 8, 9, 10]
+    [11,12,13,14,15] 
+
+    Don't forget to free it!
+   */   
+
+int main()
+{
+  // step # 1. create the double pointer that will be used to reference to
+  // the 2D matrix
+  
+
+  // step #2 . allocate enough memory for the rows (what is the data type?)
+  
+
+  // step #3 . for each one of the rows, allocate enough memory for the columns
+
+
+  // step #4 . verify that the 2D matrix works as usual, just as you saw in the
+  // previous semesters, you can use brackets [ ] fill matrix and print to test 
+  
+  //fill it
+
+  //print
+
+
+  /* Can you do the same to store the following :
+
+  ["WELCOME"]                len = 8
+  ["TO"]                     len = 3 
+  ["DATA STRUCTURE CLASS"]   len = 21
+
+  */
+
+
+  /* can you print it using pointer math :) ? */
+
+
+
+  /* can you print it with brackers [] ? */
+
+  
+
+  /* FREE everything we just created */
+
+}
