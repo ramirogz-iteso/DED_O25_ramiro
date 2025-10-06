abe2966c19c6d995086c5ed2ae4bc6c3029e1310
ramirogz <ramirogz@iteso.mx>
2025-10-06 13:27:06 -0600

    Oct6

--- malloc2d.c
+++ malloc2d.c
@@ -14,20 +14,42 @@ int main()
 {
   // step # 1. create the double pointer that will be used to reference to
   // the 2D matrix
-  
-
+  int ** matrix;
   // step #2 . allocate enough memory for the rows (what is the data type?)
-  
+  matrix = malloc ( 3 * sizeof(int *));
 
   // step #3 . for each one of the rows, allocate enough memory for the columns
-
+  //matrix[0]= (int) malloc(sizeof(int)*5);
+  //matrix[1]= (int) malloc(sizeof(int)*5);
+  //matrix[2]= (int) malloc(sizeof(int)*5);
+  for(int i = 0; i<3; i++)
+  {
+    matrix[i] = malloc(sizeof(int) * 5);
+  }
 
   // step #4 . verify that the 2D matrix works as usual, just as you saw in the
   // previous semesters, you can use brackets [ ] fill matrix and print to test 
-  
   //fill it
+  int cont=1;
+  //fill it
+  for(int x = 0; x<3; x++)
+  {
+    for(int y = 0; y<5; y++)
+    {
+      matrix[x][y] = cont;
+      cont++;
+    }  
+  }
 
   //print
+   for(int x = 0; x<3; x++)
+  {
+    for(int y = 0; y<5; y++)
+    {
+      printf("[%d]", matrix[x][y]);
+    }  
+    printf("\n");
+  }
 
 
   /* Can you do the same to store the following :
@@ -37,16 +59,33 @@ int main()
   ["DATA STRUCTURE CLASS"]   len = 21
 
   */
+  char ** banner = malloc (sizeof(char *) * 3);
 
+  banner[0] = malloc(sizeof(char) * 8);
+  banner[1] = malloc(sizeof(char) * 3);
+  banner[2] = malloc(sizeof(char) * 21);
 
-  /* can you print it using pointer math :) ? */
-
-
+  strcpy(banner[0], "WELCOME");
+  strcpy(banner[1], "TO");
+  strcpy(banner[2], "DATA STRUCTURE CLASS");
+  
+    /* can you print it using pointer math :) ? */
+  char ** charp = banner;
+  printf("%s %s %s\n", *charp, *(charp+1), *(charp+2));
 
   /* can you print it with brackers [] ? */
-
+ printf("%s %s %s\n", banner[0], banner[1], banner[2]);
   
 
   /* FREE everything we just created */
+  //
+  free(banner[0]);
+  free(banner[1]);
+  free(banner[2]);
+
+  free(banner);
+
+  // the same for the matrix
+
 
 }
