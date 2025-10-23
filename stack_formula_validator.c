bedb98e82099c6a52beb1183809665a95ee18acd
ramirogz <ramirogz@iteso.mx>
2025-10-23 10:39:26 -0600

    oct23

--- stack_formula_validator.c
+++ stack_formula_validator.c
@@ -71,16 +71,90 @@ void display()
   printf("NULL\n\n");
 }
 
+void isvalidformula_simple(char * formula)
+{
+  for(int c = 0; c<30; c++)
+  {
+    if(formula[c] == '\0')
+      break;
+
+    if(formula[c]=='(')
+    {
+      push(formula[c]);
+    }
+    else if(formula[c]==')') 
+    {
+      char popped = pop();
+      if(popped == '0')
+      { 
+        printf("ERROR, %s \n\tthis is not a valid formula\n\n", formula);
+        return;
+      }
+    }
+  }
+  if(count!=0)
+  {
+     printf("ERROR, %s \n\tthis is not a valid formula\n\n", formula);
+     printf("stack contains %d elements\n", count);
+     return;
+  }
+  // If we reach this point, the stack is empty, so
+  // it is a valid formula!
+  printf("%s is a valid formula!\n\n", formula);
+}
+
 void isvalidformula(char * formula)
 {
- 
+  for(int c = 0; c<30; c++)
+  {
+    if(formula[c] == '\0')
+      break;
+
+    if(formula[c]=='(' || formula[c]=='{')
+    {
+      push(formula[c]);
+    }
+    else if(formula[c]==')') 
+    {
+      char popped = pop();
+      if(popped == '0' || popped != '(')
+      { 
+        printf("ERROR, %s \n\tthis is not a valid formula\n\n", formula);
+        return;
+      }
+    }
+    else if(formula[c]=='}') 
+    {
+      char popped = pop();
+      if(popped == '0' || popped != '{')
+      { 
+        printf("ERROR, %s \n\tthis is not a valid formula: ", formula);
+        printf("pop = %c\n", popped);
+        return;
+      }
+    }
+  }
+  if(count!=0)
+  {
+     printf("ERROR, %s \n\tthis is not a valid formula\n\n", formula);
+     printf("stack contains %d elements\n", count);
+     return;
+  }
+  // If we reach this point, the stack is empty, so
+  // it is a valid formula!
+  printf("%s is a valid formula!\n\n", formula);
 }
 
 int main() 
 {
-  char formula1[30] = "}({}){";
-  char formula2[30] = "{{}()}{{((";
+  char formula1[30] = "(a+b)+((b))";
+  char formula2[30] = "(123))";
 
-  isvalidformula(formula1);
-  isvalidformula(formula2);
+  //char formula1[30] = "}({}){";
+  //char formula2[30] = "{{}()}{{((";
+
+  isvalidformula_simple(formula1);
+  isvalidformula_simple(formula2);
+
+  //use isvalidformula to fi
 }
